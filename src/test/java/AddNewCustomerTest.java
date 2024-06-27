5f28d95dce0073766d8f0b7b07473db3fee7908a
Lorentso Simov <lorentso.simov@gmail.com>
2024-06-27 20:15:18 +0300

    initial commit

--- src/test/java/AddNewCustomerTest.java
+++ src/test/java/AddNewCustomerTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -66,6 +67,9 @@ public class AddNewCustomerTest {
         safeSelect.selectByValue("1");
         WebElement saveButton = driver.findElement(By.cssSelector(".btn-primary"));
         saveButton.click();
+
+
+
 
 
     }
